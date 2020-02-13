e05733a75f1f2bdab7476875701489f22c17bdd0
Teo Bous <tbousiou@gmail.com>
2020-02-13 02:10:05 +0200

    More templates and sampla data

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # school-library-tutorial
+
+# initial installation
+git clone <repo-name>
+cd <reponame>
+python3 -m venv venv
+source venv/bin/activate
+
+
+# Switch to the branch
+git reset --hard
+git branch -a
+git checkout <branch-name>
+
+# To run the app
+export FLASK_ENV=development
+flask run
--- app.py
+++ app.py
@@ -3,6 +3,39 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+books_data = [
+	{'id':0, 'title':'Lord of the Rings', 'author':'J.R. Tolkien', 'category':'Fantasy', 'year': 1968},
+	{'id':1, 'title':'Pet Semetary', 'author':'Stephen King', 'category':'Horror', 'year': 1979},
+	{'id':2, 'title':'War and Peace', 'author':'Leo Tolstoy', 'category':'Fiction', 'year': 1962}
+]
+ 
+
 @app.route('/')
-def hello():
-    return render_template('base.html')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/books')
+def books():
+    return render_template('books.html', books=books_data)
+
+
+@app.route('/book/view/<int:id>')
+def book_view(id):
+    return render_template('book-view.html', book=books_data[id])
+
+
+@app.route('/members')
+def members():
+    return render_template('members.html')
+
+@app.route('/loans')
+def loans():
+    return render_template('loans.html')
+
+
+@app.route('/reports')
+def reports():
+    return render_template('reports.html')
+
+
